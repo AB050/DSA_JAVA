1a0d5881ebb0ac95a37c6d14789e8a93eed49e26
AB050 <abhinayaman94@gmail.com>
2022-09-30 18:40:21 +0530

    Modification

--- 17thSept/RichestCustomer.java
+++ 17thSept/RichestCustomer.java
@@ -1,7 +1,8 @@
 public class RichestCustomer {
   public static void main(String[] args) {
-    int[][] accounts = { { 1, 5 }, { 7, 3 }, { 3, 5 } }; // O/p--- 10
+    int[][] accounts = { { 1, 5, 6 }, { 7, 3, 4 }, { 3, 5, 5 } }; // O/p--- 10
     System.out.println(maximumWealth(accounts));
+    System.out.println(maxWealthinCOloumn(accounts));
   }
 
   private static int maximumWealth(int[][] accounts) {
@@ -10,6 +11,20 @@ public class RichestCustomer {
       sum = 0;
       for (int j = 0; j < accounts[i].length; j++) {
         sum += accounts[i][j];
+        max = Math.max(sum, max);
+      }
+    }
+    return max;
+  }
+
+  private static int maxWealthinCOloumn(int[][] accounts) {
+    int max = 0, sum = 0;
+    int rows = accounts.length;
+    int cols = accounts[0].length;
+    for (int i = 0; i < cols; i++) {
+      sum = 0;
+      for (int j = 0; j < rows; j++) {
+        sum += accounts[j][i];
         max = Math.max(sum, max);
       }
     }
